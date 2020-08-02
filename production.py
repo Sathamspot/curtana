8740678201e454c1e3c1325c53d8609e2f3af447
Priyam Kalra <43999863+justaprudev@users.noreply.github.com>
2020-08-02 14:34:26 +0530

    Prioritise custom filters, update & auth chats.

--- production.py
+++ production.py
@@ -18,6 +18,6 @@ class Config(object):
     BLACK_LIST = set(int(x) for x in os.environ.get(
         "BLACK_LIST", "").split())
     CUSTOM_FILTERS = ["Download", "XDA", "Support", "Changelog", *os.environ.get("CUSTOM_FILTERS", "").split()]
-    AUTH_CHATS = ["-1001438120125", *os.environ.get("AUTH_CHATS", "").split()]
-    UPDATE_CHATS = ["@curtanaupdates", *os.environ.get("UPDATE_CHATS", "").split()]
+    AUTH_CHATS = [*os.environ.get("AUTH_CHATS", "").split(), "-1001438120125"]
+    UPDATE_CHATS = [*os.environ.get("UPDATE_CHATS", "").split(), "@curtanaupdates"]
     BLOCKED_UPDATES = os.environ.get("BLOCKED_UPDATES", "").split()
